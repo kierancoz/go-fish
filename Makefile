ff15a86bd79cd48013a4ee53fcf86c4d448bb0cb
kierancoz <kierancoz.w@gmail.com>
2020-10-27 13:02:58 -0500

    Working unit tests

--- Makefile
+++ Makefile
@@ -6,3 +6,18 @@ build:
 debug:
 	cd build/Debug; cmake -DCMAKE_BUILD_TYPE=Debug ../../; make
 .PHONY : debug
+
+test:
+	./bin/Release/UnitTests --logger=HRF,all --color_output=true --report_format=HRF --show_progress=no
+.PHONY : test
+
+# if future test suites are added, they can all be tested with this command
+test_all:
+	cd build/Release; make test
+.PHONY : test_all
+
+demo:
+	./bin/Release/go_fish_demo
+.PHONY : demo
+
+# clean: to implement
--- test/test.cpp
+++ test/test.cpp
@@ -2,17 +2,13 @@
 // Created by kieran on 10/26/20.
 //
 
-#define BOOST_TEST_MODULE SqrTests
+#define BOOST_TEST_MODULE GoFishTests
 #include <boost/test/unit_test.hpp>
 
 #include "Enums/DMPropertyType.h"
 
-BOOST_AUTO_TEST_CASE(FailTest)
+BOOST_AUTO_TEST_CASE(CheckLibraryLinkage)
 {
+    BOOST_CHECK_EQUAL((int)DMPropertyType::DeckType, (int)DMPropertyType::DeckType);
     BOOST_CHECK_EQUAL(1, (int)DMPropertyType::Deck);
-}
-
-BOOST_AUTO_TEST_CASE(PassTest)
-{
-    BOOST_CHECK_EQUAL(1, (int)DMPropertyType::DeckType);
 }
